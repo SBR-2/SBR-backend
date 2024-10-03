23184b583a26418537bbc060f88c4d3a3172d9e4
shinso-raven <1116252@est.intec.edu.do>
2024-10-02 20:07:27 -0400

    feat: mutation inspeccion bpm

--- SBRSystem-API/GraphQl/Mutation/BpmInpeccionMutation.cs
+++ SBRSystem-API/GraphQl/Mutation/BpmInpeccionMutation.cs
@@ -127,14 +127,126 @@ public class BpmInpeccionMutation
 
         return context.Pregunta.First(x => x.Nombre ==preguntaInput.Nombre);
    }
+
+   public async Task<Respuestum> AddRespuesta(AddRespuestaInput input , [Service] MySBRDbContext context)
+   {
+       Respuestum newRespuesta = new Respuestum
+       {
+           PreguntaId = input.PreguntaId,
+           FichaId = input.FichaId,
+           ValorId = input.ValorId,
+           Descripcion = input.Descripcion,
+           Estado = input.Estado,
+       };
+       
+        try
+        {
+            context.Respuesta.Add(newRespuesta);
+            context.SaveChanges();
+
+        }
+        catch (Exception e)
+        {
+            throw new GraphQLException("Ocurrió un error al agregar la pregutna.");
+        }
+
+        return context.Respuesta
+            .First(x => x.PreguntaId == input.PreguntaId && x.FichaId == input.FichaId);
+   }
+
+   public async Task<Ficha> AddFicha(AddFichaInput input, [Service] MySBRDbContext context)
+   {
+       Ficha newFicha = new Ficha
+       {
+           SolicitudId = input.SolicitudId,
+           EstablecimientoId = input.EstablecimientoId,
+           Fecha = input.Fecha,
+           Estado = input.Estado,
+       };
+
+       try
+       {
+           context.Fichas.Add(newFicha);
+           context.SaveChanges();
+
+       }
+       catch (Exception e)
+       {
+           throw new GraphQLException("Ocurrio un error al agregar una ficha");
+       }
+
+       return context.Fichas.First(x =>
+           x.SolicitudId == input.SolicitudId && x.EstablecimientoId == input.EstablecimientoId);
+
+   }
+   
+   public async Task<Valor> AddValor(AddValorInput input, [Service] MySBRDbContext context)
+   {
+       Valor newValor = new Valor
+       {
+           NomenclaturaValor = input.NomenclaturaValor,
+           Puntos = input.Puntos,
+           Descripcion = input.Descripcion,
+           Estado = input.Estado,
+       };
+
+       try
+       {
+           context.Valors.Add(newValor);
+           context.SaveChanges();
+
+       }
+       catch (Exception e)
+       {
+           throw new GraphQLException("Ocurrio un error al agregar un valor");
+       }
+
+       return context.Valors.First(x =>
+           x.NomenclaturaValor== input.NomenclaturaValor);
+
+   }
+   
+   public async Task<Establecimiento> AddEstablecimiento(AddEstablecimientoInput input, [Service] MySBRDbContext context)
+   {
+       Establecimiento newEstablecimiento = new Establecimiento
+       {
+           Nombre = input.Nombre,
+           Numero = input.Numero,
+           Municipio = input.Municipio,
+           Provincia = input.Provincia,
+           Telefono = input.Telefono,
+           InicioOperaciones = input.InicioOperaciones,
+           VencimientoSanitario = input.VencimientoSanitario,
+           Rnc = input.Rnc,
+           NumProductosElaborados = input.NumProductosElaborados,
+           ProduccionAnual = input.ProduccionAnual,
+           Comercializacion = input.Comercializacion,
+           MercadoObjetivo = input.MercadoObjetivo,
+           NumEmpleados = input.NumEmpleados,
+           UltimaInspeccion = input.UltimaInspeccion,
+           CalUltimaInspeccion = input.CalUltimaInspeccion,
+           NombreDps = input.NombreDps,
+           NombreDigemaps = input.NombreDigemaps,
+           NoSanitario = input.NoSanitario,
+       };
+
+       try
+       {
+           context.Establecimientos.Add(newEstablecimiento);
+           context.SaveChanges();
+
+       }
+       catch (Exception e)
+       {
+           throw new GraphQLException("Ocurrio un error al agregar un establecimiento");
+       }
+
+       return context.Establecimientos.First(x =>
+           x.Rnc== input.Rnc);
+
+   }
    
    
-   /*
-    * Agregar respuesta
-    * Agregar Ficha
-    * Agregar establecimiento
-    *
-    */
    
    
    
--- SBRSystem-API/GraphQl/input/BPMInspeccionMutationInput.cs
+++ SBRSystem-API/GraphQl/input/BPMInspeccionMutationInput.cs
@@ -42,3 +42,78 @@ public class AddPregunta: AddInputBPM
     
     public int BpmSubcategoriaId { get; set; }
 }
+
+public class AddRespuestaInput
+{
+    public int PreguntaId { get; set; }
+
+    public int FichaId { get; set; }
+
+    public int ValorId { get; set; }
+
+    public string Descripcion { get; set; }
+
+    public bool Estado { get; set; }
+}
+
+public class AddFichaInput
+{
+    public int SolicitudId { get; set; }
+
+    public int EstablecimientoId { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public bool Estado { get; set; }
+}
+
+public class AddValorInput
+{
+    public string NomenclaturaValor { get; set; }
+
+    public int Puntos { get; set; }
+
+    public string Descripcion { get; set; }
+
+    public bool Estado { get; set; }
+}
+
+public class AddEstablecimientoInput
+{
+    
+    public string  Nombre { get; set; }
+
+    public string  Numero { get; set; }
+
+    public string  Municipio { get; set; }
+
+    public string  Provincia { get; set; }
+
+    public string  Telefono { get; set; }
+
+    public DateTime  InicioOperaciones { get; set; }
+
+    public DateTime  VencimientoSanitario { get; set; }
+
+    public string  Rnc { get; set; }
+
+    public int  NumProductosElaborados { get; set; }
+
+    public int  ProduccionAnual { get; set; }
+
+    public string  Comercializacion { get; set; }
+
+    public string  MercadoObjetivo { get; set; }
+
+    public int  NumEmpleados { get; set; }
+
+    public DateTime  UltimaInspeccion { get; set; }
+
+    public long  CalUltimaInspeccion { get; set; }
+
+    public string  NombreDps { get; set; }
+
+    public string  NombreDigemaps { get; set; }
+
+    public bool  NoSanitario { get; set; }
+}
