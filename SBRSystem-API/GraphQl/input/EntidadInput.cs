7449eb59d1b0286c1e1bfb66ec580ad8a37d8eb5
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2024-10-16 23:32:42 -0400

    Update EntidadInput.cs

--- SBRSystem-API/GraphQl/input/EntidadInput.cs
+++ SBRSystem-API/GraphQl/input/EntidadInput.cs
@@ -2,7 +2,7 @@ namespace SBRSystem_API.GraphQl;
 
 public class AddEntidadInput
 {
-    [GraphQLNonNullType] public string? Nombre { get; set; }
+    public string? Nombre { get; set; }
     public string? Direccion { get; set; }
     public string? Telefono { get; set; }
     public string? Correo { get; set; }
